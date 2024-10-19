c5a9768df33f86f84f437cb730c5e1aa2a8eb60e
Lim Jet <57783762+daoauth@users.noreply.github.com>
2024-10-19 12:57:40 +0900

    epochs example

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="/walrus-testnet.jpeg" className="App-logo" alt="logo" />
+        <img src="/walrus-testnet.png" className="App-logo" alt="logo" />
         <p>
           Walrus Sites GA Example
         </p>
